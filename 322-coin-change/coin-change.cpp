d99d90c5f55e5e5a407d5d3d3d1745848d87c188
Madhava Kandala <149293303+MadhavaKandala@users.noreply.github.com>
2025-05-18 20:02:20 +0530

    Time: 16 ms (86.72%) | Memory: 17.8 MB (92.10%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -3,7 +3,7 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         vector<int> dp(amount + 1, amount + 1);
         dp[0] = 0;
-
+    
         for (int i = 1; i <= amount; ++i) {
             for (int coin : coins) {
                 if (i - coin >= 0) {
@@ -11,7 +11,7 @@ public:
                 }
             }
         }
-
-        return dp[amount] <= amount ? dp[amount] : -1;
-    }   
+    
+        return dp[amount] > amount ? -1 : dp[amount];
+}   
 };
