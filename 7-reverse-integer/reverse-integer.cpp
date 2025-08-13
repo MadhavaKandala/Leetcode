8d8b79c53cccfe55a8cc6cb024c1675ee150a044
Madhava Kandala <149293303+MadhavaKandala@users.noreply.github.com>
2025-08-13 21:22:31 +0530

    Time: 5 ms (19.90%) | Memory: 8.3 MB (99.93%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,12 +1,22 @@
 class Solution {                      
 public:
     int reverse(int x) {
-        long r=0;      // decleare r 
+        long rev=0;      // decleare r 
         while(x){
-         r=r*10+x%10; // find remainder and add its to r
-         x=x/10;     // Update the value of x
+            long rem = x % 10;
+            rev = rev * 10 + rem;
+            x /= 10;            
+        //  r=r*10+x%10; // find remainder and add its to r
+        //  x=x/10;     // Update the value of x
         }
-        if(r>INT_MAX || r<INT_MIN) return 0; // check range if r is outside the range then return 0  
-        return int(r);  // if r in the 32 bit range then return r
+        if(rev>INT_MAX || rev<INT_MIN) return 0; // check range if r is outside the range then return 0  
+        return int(rev);  // if r in the 32 bit range then return r
     }
 }; 
+
+
+// 123
+
+// 1.    rem = 3,   rev = 3
+// 2.    rem = 2    rev = 32
+// 3.    rem = 1    rev = 321;
